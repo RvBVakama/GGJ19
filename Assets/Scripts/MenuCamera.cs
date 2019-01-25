7e7aca1ac80ee4567ce666651d003e980e6d71c1
RvBVakama <karate.1996@hotmail.com>
2019-01-26 09:33:15 +1100

    Added post processing stack package, menu sceen basic layout in.

---
+++ Assets/Scripts/MenuCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamera : MonoBehaviour
+{
+    public GameObject target = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform.position);
+    }
+}
