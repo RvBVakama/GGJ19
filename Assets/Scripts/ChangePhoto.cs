9cda10cccf7d39859a24861b91ce5059b742db45
RvBVakama <karate.1996@hotmail.com>
2019-01-26 02:39:57 +1100

    can change photos now, code works, thanks to kyle staves

--- Assets/Scripts/ChangePhoto.cs
+++ Assets/Scripts/ChangePhoto.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChangePhoto : MonoBehaviour
 {
+    public Texture[] frames;
+
+    public void Start()
+    {
+        frames = new Texture[100];
+        Object[] textures = Resources.LoadAll("AwesomeImages/DesiredImages", typeof(Texture2D));
+
+        for (int i = 0; i < textures.Length; i++)
+        {
+            frames[i] = (Texture)textures[i];
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        ChangePicture();
+        CyclePhoto();
     }
 
-    public void ChangePicture()
+    public void CyclePhoto()
     {
         // if mouse button left or right
         if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
@@ -22,24 +36,31 @@ public class ChangePhoto : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                if (hit.transform.tag == "Photo")
-                {
-                    // if mouse button left
-                    if (Input.GetMouseButtonDown(0))
-                    {
-                        Debug.Log("changeleft");
-                    }
-
-                    // if mouse button right
-                    if (Input.GetMouseButtonDown(1))
-                    {
-                        Debug.Log("changeright");
-                    }
-                }
+                Cycle(hit);
             }
         }
         //RawImage ri;
         //ri.mainTexture.width
+    }
+
+    public void Cycle(RaycastHit hit)
+    {
+        RawImage Ri;
+
+        if (Ri = hit.transform.GetComponent<RawImage>())
+        {
+            // if mouse button left
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ri.texture = frames[0];
+            }
+
+            // if mouse button right
+            if (Input.GetMouseButtonDown(1))
+            {
+
+            }
+        }
     }
 }
 
