c3d8a8d8664d6b51406933cdf4531d94e4f44d3a
NerbzHub <nathan.nette@gmail.com>
2019-01-26 16:01:00 +1100

    Added the sand and water for the underground oasis.

---
+++ Assets/Scripts/AssignCursor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AssignCursor : MonoBehaviour
+{
+    public Texture2D cursorTex = null;
+
+    // Use this for initialization
+    void Awake()
+    {
+        Cursor.SetCursor(cursorTex, Vector2.zero, CursorMode.Auto);
+        Debug.Log("hit enter");
+    }
+//    void OnMouseExit()
+//    {
+//        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+//        Debug.Log("hit exit");
+//    }
+}
